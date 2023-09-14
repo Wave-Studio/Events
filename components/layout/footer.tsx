17c09c28e9dcd699b2395576b4ce98b147e7ca53
Lukas <69743171+quick007@users.noreply.github.com>
2023-09-14 14:01:04 -0700

    Update footer.tsx

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,17 +1,34 @@
 const Footer = () => {
+
+  const links: {name: string, link: string}[] = [
+    {
+      name: "faq",
+      link: "/faq"
+    },
+    {
+      name: "privacy",
+      link: "/privacy-policy"
+    },
+    {
+      name: "terms",
+      link: "/terms-of-service"
+    }
+  ]
+
   return (
     <div class="flex flex-col items-center text-sm">
       <p class="flex font-bold">
         a{" "}
-        <span class="mx-1 bg-gradient-to-b from-[#234c70] to-[#0a3860] text-transparent bg-clip-text">
+        <a href="https://wave-studios.netlify.app/" target="_blank" class="mx-1 bg-gradient-to-b from-[#234c70] to-[#0a3860] text-transparent bg-clip-text hover:brightness-200 transition duration-300">
           Wave Studios
-        </span>{" "}
+        </a>{" "}
         project
       </p>
       <div className="flex my-2 underline gap-8 font-medium">
-        <a>faq</a>
-				<a>privacy</a>
-				<a>terms</a>
+        {links.map(link => (
+          <a href={link.link}>{link.name}</a>
+        ))}
+        
       </div>
     </div>
   );
