bc9cf487643bcfee1add174a08d079f1d6e19d8e
Lukas <69743171+quick007@users.noreply.github.com>
2023-09-27 21:42:49 -0700

    Start page 2 📃

--- components/buttons/toggle.tsx
+++ components/buttons/toggle.tsx
@@ -2,13 +2,15 @@ import { StateUpdater } from "preact/hooks";
 
 // basic switch if we ever need to use it
 export const Switch = ({
+enabled,
   setEnabled,
 }: {
+  enabled: boolean;
   setEnabled: StateUpdater<boolean>;
 }) => {
   return (
-    <div>
-      <div></div>
+    <div class="rounded-full w-12 min-w-[3rem] border border-gray-300 flex items-center cursor-pointer" onClick={() => setEnabled(e => !e)}>
+      <div class={`h-5 w-5 m-0.5 ${enabled ? "bg-theme-normal translate-x-[1.425rem]" : "bg-gray-200"} transition duration-200 rounded-full`} />
     </div>
   );
 };
@@ -16,21 +18,23 @@ export const Switch = ({
 
 // drop in component version of switch
 export const Toggle = ({
+  enabled,
   setEnabled,
   name,
   description,
 }: {
+  enabled: boolean
   setEnabled: StateUpdater<boolean>;
   name: string;
   description?: string;
 }) => {
   return (
-    <div class="flex">
-      <div>
-        <h3>{name}</h3>
-        {description}
+    <div class="flex items-center justify-between grow">
+      <div class="mr-4">
+        <h3 class="text-sm font-medium mb-0.5">{name}</h3>
+        <p class="text-xs text-gray-500">{description}</p>
       </div>
-      <Switch setEnabled={setEnabled} />
+      <Switch setEnabled={setEnabled} enabled={enabled} />
     </div>
   );
 };
--- islands/events/two.tsx
+++ islands/events/two.tsx
@@ -7,6 +7,7 @@ import PhotoPlus from "$tabler/photo-plus.tsx";
 import { StateUpdater, useMemo, useState } from "preact/hooks";
 import CalenderPicker from "@/components/pickers/calender.tsx";
 import TimePicker from "@/components/pickers/time.tsx";
+import { Toggle } from "@/components/buttons/toggle.tsx";
 
 export default function StageTwo({
   eventState,
@@ -15,17 +16,45 @@ export default function StageTwo({
   eventState: Signal<Event>;
   setPage: StateUpdater<number>;
 }) {
-  const { showTimes } = eventState.value;
+  const { multiEntry, maxTickets, additionalFields } = eventState.value;
+  const [mEntry, setMEntry] = useState(multiEntry);
 
   return (
     <>
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-        page 2
-        <div className="flex justify-between md:col-span-2">
-            <CTA btnType="secondary" btnSize="sm" className="!w-20 md:!w-40" type="button" onClick={() => setPage(1)}>
-              Back
-            </CTA>
-          <CTA btnType="cta" btnSize="sm" className="!w-20 md:!w-40" onClick={() => {}}>
+      <div class="flex flex-col gap-4">
+        <div className="flex gap-4 grow">
+          <Toggle
+            name="Allow Multi-Entry"
+            description="Allow attendees to enter multiple times on the same ticket"
+            setEnabled={setMEntry}
+            enabled={mEntry}
+          />
+          <label htmlFor="" class="flex flex-col w-40">
+            <p className="label-text">Maximum Attendees</p>
+            <input type="number" class="p-2 border rounded-md border-gray-300" pattern="\d*" />
+          </label>
+        </div>
+				<h3 class="font-medium text-center">
+					Additional Fields
+				</h3>
+				<h4 class="text-sm font-medium">Required Fields</h4>
+				<h4 class="text-sm font-medium">Additional Fields</h4>
+        <div className="flex justify-between">
+          <CTA
+            btnType="secondary"
+            btnSize="sm"
+            className="!w-20 md:!w-40"
+            type="button"
+            onClick={() => setPage(1)}
+          >
+            Back
+          </CTA>
+          <CTA
+            btnType="cta"
+            btnSize="sm"
+            className="!w-20 md:!w-40"
+            onClick={() => {}}
+          >
             Next
           </CTA>
         </div>
