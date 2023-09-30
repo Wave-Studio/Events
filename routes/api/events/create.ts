ef1fbde321cad58365a25e312b64b34162901322
Lukas <69743171+quick007@users.noreply.github.com>
2023-09-29 21:38:22 -0700

    atomic

--- routes/api/events/create.ts
+++ routes/api/events/create.ts
@@ -57,7 +57,7 @@ export const handler: Handlers = {
     }
 
     const event: Event = await req.json();
-		console.log(event)
+    // console.log(event)
     const eventID = crypto.randomUUID();
 
     try {
@@ -69,26 +69,23 @@ export const handler: Handlers = {
     }
 
     // I feel like this may not be the best way to do this
-    const response = await Promise.all([
-      await kv.set(["event", eventID], {
+    const response = await kv
+      .atomic()
+      .set(["event", eventID], {
         ...event,
         members: [{ email: user.email, role: Roles.OWNER }],
-      } as Event),
-
-      await kv.set(["user", user.email], {
+      } as Event)
+      .set(["user", user.email], {
         ...user,
         events: [...user.events, eventID],
-      } as User),
-    ]);
+      } as User)
+      .commit();
 
-		if (response.every(res => res.ok)) {
-			return new Response(
-				JSON.stringify({ success: true, eventID }),
-				{
-					status: 200,
-				}
-			);
-		}
+    if (response.ok) {
+      return new Response(JSON.stringify({ success: true, eventID }), {
+        status: 200,
+      });
+    }
 
     return new Response(
       JSON.stringify({ error: "Lukas was too lazy to finish this - Bloxs" }),
