41624df41b86f5261c3a156ca67d498de9e7818a
Blocks <51055767+Blocksnmore@users.noreply.github.com>
2023-10-06 20:22:31 -0500

    Add env example and basic readme

--- README.md
+++ README.md
@@ -1,16 +1,6 @@
-# Fresh project
+# Events
+Event Ticketing of the future
 
-Your new Fresh project is ready to go. You can follow the Fresh "Getting
-Started" guide here: https://fresh.deno.dev/docs/getting-started
+## Hosting
 
-### Usage
-
-Make sure to install Deno: https://deno.land/manual/getting_started/installation
-
-Then start the project:
-
-```
-deno task start
-```
-
-This will watch the project directory and restart as necessary.
+Events is built with [Deno Deploy](https://deno.com/deploy) support in mind but you should be able to selfhost this anywhere, just copy `.env.example` to `.env` and update the variables
--- utils/imagekit.ts
+++ utils/imagekit.ts
@@ -1,10 +1,13 @@
 // @deno-types="npm:@types/imagekit"
 import ImageKit from "imagekit";
 
-const imageKit = new ImageKit({
-  publicKey: Deno.env.get("IMAGEKIT_PUBLIC_KEY")!,
-  privateKey: Deno.env.get("IMAGEKIT_PRIVATE_KEY")!,
-  urlEndpoint: Deno.env.get("IMAGEKIT_URL_ENDPOINT")!,
-});
+const isImagekitEnabled =
+  Deno.env.get("IMAGEKIT_PUBLIC_KEY")?.toLowerCase() == "true";
 
-export default imageKit;
+export default isImagekitEnabled
+  ? new ImageKit({
+    publicKey: Deno.env.get("IMAGEKIT_PUBLIC_KEY")!,
+    privateKey: Deno.env.get("IMAGEKIT_PRIVATE_KEY")!,
+    urlEndpoint: Deno.env.get("IMAGEKIT_URL_ENDPOINT")!,
+  })
+  : undefined;
