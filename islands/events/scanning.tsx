335a99faaf024505920bf2be5c9e1c91775a204a
Blocks <51055767+Blocksnmore@users.noreply.github.com>
2023-10-23 18:05:54 -0500

    Send help

--- islands/events/scanning.tsx
+++ islands/events/scanning.tsx
@@ -15,15 +15,15 @@ export default function Scanner({ className }: { className?: string }) {
       if (initialized) return;
       setInitialized(true);
       const canvas = document.getElementById("scanui") as HTMLCanvasElement;
-      const barcodeReaderAPI = window["BarcodeDetector"] ?? BarcodeDetector;
-      if (barcodeReaderAPI == null) {
-        return setError(
-          "BarcodeDetector API is required but not supported on your device! Please try another browser.",
-        );
-      }
-      const reader = new barcodeReaderAPI({
-        formats: ["qr_code"],
-      });
+      //   const barcodeReaderAPI = window["BarcodeDetector"] ?? BarcodeDetector;
+      //   if (barcodeReaderAPI == null) {
+      //     return setError(
+      //       "BarcodeDetector API is required but not supported on your device! Please try another browser.",
+      //     );
+      //   }
+      //   const reader = new barcodeReaderAPI({
+      //     formats: ["qr_code"],
+      //   });
       if (canvas == null) return;
       const ctx = canvas.getContext("2d", {
         willReadFrequently: true,
@@ -45,27 +45,60 @@ export default function Scanner({ className }: { className?: string }) {
         video.srcObject = devices;
         video.onloadedmetadata = () => {
           video.play();
+          const container = document.getElementById("scale-factor")!;
+          canvas.width = container.clientWidth;
+          canvas.height = container.clientHeight;
 
           const checkedCodes: string[] = [];
 
           const lookForBarcodes = async () => {
-            const codes = await reader.detect(video);
-            if (codes.length > 0) {
-              for (const code of codes) {
-                if (checkedCodes.includes(code.rawValue)) continue;
-                checkedCodes.push(code.rawValue);
-                console.log(code);
-              }
-            }
+            // const codes = await reader.detect(video);
+            // if (codes.length > 0) {
+            //   for (const code of codes) {
+            //     if (checkedCodes.includes(code.rawValue)) continue;
+            //     checkedCodes.push(code.rawValue);
+            //     console.log(code);
+            //   }
+            // }
           };
+
+		  // TODO: @quick007 Fix this because our brains are cooking in their own fluids - Bloxs
+          console.log(
+            "Video:",
+            video.videoWidth,
+            video.videoHeight,
+
+            "Canvas:",
+            canvas.width,
+            canvas.height,
+
+            "Meth:",
+            (video.videoWidth - canvas.width) / 2,
+            (video.videoHeight - canvas.height) / 2,
+
+            "Meth 2 electric boogaloo:",
+            video.videoWidth - (video.videoWidth - canvas.width) / 2,
+            video.videoHeight - (video.videoHeight - canvas.height) / 2,
+
+            "Aspects: ",
+			((video.videoWidth - (video.videoWidth - canvas.width) / 2) - ((video.videoWidth - canvas.width) / 2))
+			/
+			((video.videoHeight - (video.videoHeight - canvas.height) / 2) - ((video.videoHeight - canvas.height) / 2)),
+
+
+            canvas.width / canvas.height,
+          );
 
           const loop = () => {
             ctx.drawImage(
               video,
-              0,
-              0,
-              video.videoWidth,
-              video.videoHeight,
+
+              (video.videoWidth - canvas.width) / 2,
+              (video.videoHeight - canvas.height) / 2,
+
+              video.videoWidth - (video.videoWidth - canvas.width) / 2,
+              video.videoHeight - (video.videoHeight - canvas.height) / 2,
+
               0,
               0,
               canvas.width,
@@ -90,14 +123,16 @@ export default function Scanner({ className }: { className?: string }) {
     <>
       <video
         id="camera"
-        //hidden={true}
+        hidden={true}
         autoPlay={true}
         playsInline={true}
         muted={true}
         className="border-none"
       />
       {error}
-      <canvas id="scanui" className={className}></canvas>
+      <div id="scale-factor" className="h-[30rem] w-[30rem]">
+        <canvas id="scanui" className={className}></canvas>
+      </div>
     </>
   );
 }
