ef1d0dcdc7737f203413056161b18af12da9a479
Lukas <69743171+quick007@users.noreply.github.com>
2023-11-14 18:21:03 -0800

    confirm build error issue
    Co-Authored-By: Bloxs <51055767+Blocksnmore@users.noreply.github.com>

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,22 +1,15 @@
 import { defineConfig } from "$fresh/server.ts";
 import unocssPlugin from "@/utils/plugins/unocss.ts";
 import {
-  createQueueValueHandler,
   kvInsightsPlugin,
 } from "https://deno.land/x/deno_kv_insights@v0.7.0-beta/mod.ts";
-import { kv } from "./utils/db/kv.ts";
-
-const kvInsightsQueueValueHandler = createQueueValueHandler();
-
-kv.listenQueue(async (value: unknown) => {
-  await kvInsightsQueueValueHandler(value); // execute the kv-insights value handler
-  // add your code to handle the queue value here
-});
+import { kv } from "@/utils/db/kv.ts";
 
 export default defineConfig({
-  plugins: [unocssPlugin()
-    // kvInsightsPlugin({
-    //  kv
-    // })
+  plugins: [
+    unocssPlugin(),
+    kvInsightsPlugin({
+      kv,
+    }),
   ],
 });
