f827c723f80f66bdf4feae0957a1e485ce27a494
Blocks <51055767+Blocksnmore@users.noreply.github.com>
2023-12-02 16:55:48 -0600

    Add autoprefixer so lukas can stop complaining

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from "$fresh/server.ts";
-import unocssPlugin from "@/utils/plugins/unocss.ts";
-import tailwind from "$fresh/plugins/tailwind.ts";
+import tailwind from "./plugins/tailwind.ts";
 import { kvInsightsPlugin } from "https://deno.land/x/deno_kv_insights@v0.7.0-beta/mod.ts";
 import { kv } from "@/utils/db/kv.ts";
 
 
 export default defineConfig({
   plugins: [
-    unocssPlugin(),
     kvInsightsPlugin({
       kv,
     }),
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,4 +16,5 @@ export default {
       },
     },
   },
+  plugins: []
 } as Config;
