a29855df1ab86c7299e8a7b176a4e2a0f6b0457d
Lukas <69743171+quick007@users.noreply.github.com>
2024-01-16 17:14:35 -0800

    Finish login code email

--- emails/login.tsx
+++ emails/login.tsx
@@ -9,39 +9,57 @@ import {
   Section,
   Tailwind,
   Text,
+  Preview,
+  Font,
+  Column,
+  Row,
 } from "@react-email/components";
 import * as React from "react";
-import config from "../tailwind.config.ts"
+import config from "../tailwind.config.ts";
 
 export default function LoginEmail({ otp = "123456" }: { otp: string }) {
   return (
     <Html>
-      <Head />
-      <Tailwind config={{
-        theme: config.theme
-      }}>
-        <Body className="bg-white">
-          <Container className="">
+      <Head>
+        <Font
+          fontFamily="Inter"
+          fallbackFontFamily="sans-serif"
+          webFont={{
+            url: "https://github.com/google/fonts/raw/main/ofl/inter/Inter%5Bslnt,wght%5D.ttf",
+            format: "woff2",
+          }}
+          fontWeight={400}
+          fontStyle="normal"
+        />
+      </Head>
+      <Tailwind
+        config={{
+          theme: config.theme,
+        }}
+      >
+        <Body className="bg-white text-gray-800 text-center">
+          <Preview>Your autorication code to log in to Events</Preview>
+          <Container className="my-8">
             <Img
               src={`https://events.deno.dev/orange-logo.svg`}
-              width="212"
-              height="88"
+              width="40"
+              height="40"
               alt="Plaid"
+              className="mx-auto"
             />
-            <Text >Verify Your Identity</Text>
-            <Heading >
-              Enter the following code to finish linking Venmo.
+            <Heading className="text mt-2 text-2xl font-bold">Events</Heading>
+            <Heading className="mt-10 mb-2">Verify Your Identity</Heading>
+            <Heading className="text-lg font-medium mt-0">
+              Enter the following code to finish logging in.
             </Heading>
-            <Section>
-              <Text>{otp}</Text>
-            </Section>
-            <Text>Not expecting this email?</Text>
+            {/* Some of the more jank classes I've written */}
+            <Text className="text-2xl font-medium mt-20 mb-44 bg-gray-200 rounded-md w-max mx-auto tracking-[1em] pl-6 pr-[calc(1.5rem-1em)] py-3 ">
+              {otp}
+            </Text>
+
             <Text>
-              Contact{" "}
-              <Link href="mailto:login@plaid.com">
-                login@plaid.com
-              </Link>{" "}
-              if you did not request this code.
+              You can safly ignore this email if you did not request this login
+              code.
             </Text>
           </Container>
         </Body>
@@ -49,4 +67,3 @@ export default function LoginEmail({ otp = "123456" }: { otp: string }) {
     </Html>
   );
 }
-
